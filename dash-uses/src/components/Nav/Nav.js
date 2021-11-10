f0c627ca26704a5467830e9d761a42a8d8b53238
Rian Islam <rianislamdr@gmail.com>
2021-11-11 01:25:48 +0600

    create a responsive navbar

---
+++ dash-uses/src/components/Nav/Nav.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Nav.css'
+const Nav = () => {
+    return (
+        <div>
+            <nav>
+                <label className="logo">Design</label>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Home</a></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Nav
