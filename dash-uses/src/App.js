68e977b8bf4e7e09f5b37d4926f04c9c915ce513
Rian Islam <rianislamdr@gmail.com>
2021-11-12 03:03:48 +0600

    add some bootstrap class

--- dash-uses/src/App.js
+++ dash-uses/src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import BootStrapNav from './components/Bootstrap/BootStrapNav';
 import Card from './components/Card/Card';
 import HexagonCard from './components/Card/HexagonCard';
 import Nav from './components/Nav/Nav';
@@ -7,7 +8,7 @@ import Nav from './components/Nav/Nav';
 function App() {
   return (
     <div>
-
+      <BootStrapNav/>
      <Card/>
      {/* <HexagonCard/> */}
     </div>
