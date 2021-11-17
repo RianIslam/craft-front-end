63e68f282d046f178ad27db49b5278432cd0bcf8
Rian Islam <rianislamdr@gmail.com>
2021-11-17 21:28:20 +0600

    add a new card

--- dash-uses/src/components/Data/Data.js
+++ dash-uses/src/components/Data/Data.js
@@ -1,29 +1,48 @@
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaTwitterSquare } from "react-icons/fa";
+
 export const data =[
 
-  {title: "3d logo",
-  id:1,
-      img: "https://images.unsplash.com/photo-1591382696684-38c427c7547a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-      btnContent:"like",
-
-  },
-  {title: "3d logo",
-  id:2,    
-  img: "https://images.unsplash.com/photo-1461673552572-51af4cbbe51a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-      btnContent:"like",
-
-  },
-  {title: "3d logo",
-  id:3,    
-  img: "https://images.unsplash.com/photo-1479658744930-03854858a953?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=721&q=80",
-      btnContent:"like",
-
-  },
-  {title: "3d logo",
-  id:4,    
-  img: "https://images.unsplash.com/photo-1591382696684-38c427c7547a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-      btnContent:"like",
-
-  },
- 
-
-]
+    {title: "B2B Lead Generation",
+    id:1,
+    img:"https://images.unsplash.com/photo-1577511757985-6c9f8518fc7b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    content:"Chief Executive Officer",
+    iconFacebook:<FaFacebook/>,
+    iconTwitter:<FaInstagram/>,
+    iconLinkedin:<FaLinkedin/>,
+    iconInsta:<FaTwitterSquare/>
+  
+    },
+    {title: "Ecom Customer Support",
+    id:2, 
+    img:"https://images.unsplash.com/photo-1577511757985-6c9f8518fc7b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    content:"Head Of Creatives" ,
+    iconFacebook:<FaFacebook/>,
+    iconTwitter:<FaInstagram/>,
+    iconLinkedin:<FaLinkedin/>,
+    iconInsta:<FaTwitterSquare/>
+  
+    },
+    {title: "Real Estate VA Services",
+    id:3,    
+    img:"https://images.unsplash.com/photo-1577511757985-6c9f8518fc7b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    content:"Head Of IT",
+    iconFacebook:<FaFacebook/>,
+    iconTwitter:<FaInstagram/>,
+    iconLinkedin:<FaLinkedin/>,
+    iconInsta:<FaTwitterSquare/>
+    },
+    {title: "Real Estate VA Services",
+    id:4,    
+    img:"https://images.unsplash.com/photo-1577511757985-6c9f8518fc7b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    content:"Head Of Marketing",
+    iconFacebook:<FaFacebook/>,
+    iconTwitter:<FaInstagram/>,
+    iconLinkedin:<FaLinkedin/>,
+    iconInsta:<FaTwitterSquare/>
+    },
+   
+  
+  ]
