84a0870cfab6031189db692876c9ee598bd2b1ab
Rian Islam <rianislamdr@gmail.com>
2021-11-30 23:16:14 +0600

    card fixing

--- dash-uses/src/components/Card/ProfileCard.js
+++ dash-uses/src/components/Card/ProfileCard.js
@@ -7,16 +7,16 @@ const ProfileCard = () => {
   return (
     <div className="container">
       <div className="row">
-      <div className="col-md-6">
-        <h1 className="text-center" >Hover card</h1>
-      </div>
-      <div className="col-md-6">
-      <div className="row ">
-          {data.map((item, index) => (
-            <HoverCard index={index} item={item} />
-          ))}
+        <div className="col-md-6">
+          <h1 className="text-center">Hover card</h1>
         </div>
-      </div>
+        <div className="col-md-6 padding-Hover">
+          <div className="row ">
+            {data.map((item, index) => (
+              <HoverCard index={index} item={item} />
+            ))}
+          </div>
+        </div>
       </div>
 
       <div className="py-5">
