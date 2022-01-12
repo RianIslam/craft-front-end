7754314c1d88582abecc1aecfe36c6e0c2377c98
Rian Islam <rianislamdr@gmail.com>
2022-01-13 05:05:01 +0600

    working on bootstrap in typescript project

--- typescript-bootstrap-app/src/components/Header.tsx
+++ typescript-bootstrap-app/src/components/Header.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Container, Navbar } from "react-bootstrap";
 
-const Header = () => {
-  return <div></div>;
+interface Props {}
+
+const Header = (props: Props) => {
+  return (
+    <div>
+      <Navbar fixed="top" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand>React Typescript Bootstrap Tuitorial</Navbar.Brand>
+        </Container>
+      </Navbar>
+    </div>
+  );
 };
 
 export default Header;
