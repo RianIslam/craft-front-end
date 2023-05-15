547fab90b7c8e9ccf48c3591a8f1a86525982aa3
Rian Islam <rianislamdr@gmail.com>
2023-05-15 10:45:40 +0600

    describe about app

--- dash-uses/src/components/Data/CoffeeData.js
+++
@@ -1,6 +0,0 @@
-export const coffee =[
-    {
-        content:"Coffee is a brewed drink prepared from roasted coffee beans, the seeds of berries from certain Coffea species.",
-        buttonContent:"Buy Now"
-    }
-]
--- typescript-bootstrap-app/src/components/CreateNotes.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const CreateNotes = () => {
-  return <div></div>;
-};
-
-export default CreateNotes;
