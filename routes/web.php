4ddfc6fe02f4dfa8d6f6379ea940a484590c7ab8
Rian Islam <rianislamdr@gmail.com>
2023-07-25 22:00:14 +0600

    delete

--- routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Http\Controllers\AuthenticationController;
-use Illuminate\Support\Facades\Route;
-
-
-Route::post('/UserRegistration',[AuthenticationController::class,'UserRegistration']);
-Route::post('/UserLogin',[AuthenticationController::class,'UserLogin']);
